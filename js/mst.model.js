f1db807fad84a3fffc8ec19bfefe15d7e8d2eb11
douglass.matthews@monster.com <douglass.matthews@monster.com>
2020-05-18 17:01:53 +0200

    style="display:none"

--- js/mst.model.js
+++ js/mst.model.js
@@ -272,7 +272,7 @@ MST.Model.Profile = Backbone.Model.extend({
 	getRefineFormData : function() {
 		var $this = this;
 		var refineData = {};
-		refineData[58061] = {
+		refineData[5000026] = {
 			zipcode : $this.getLocation(),
 			keyword : $this.getKeyword(),
 			size : $this.getSize(),
